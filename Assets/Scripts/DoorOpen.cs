f64df5162ab5dee12a94cf8a2785651e32186f0d
Workshop User <workshop@efclassroom.idmt.fraunhofer.de>
2017-10-13 11:32:50 +0200

    Add door script

---
+++ Assets/Scripts/DoorOpen.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour {
+
+	public int TurningValue = 0;
+	public int MaxTurningValue = 0;
+	public float TurningSpeed = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (TurningValue < MaxTurningValue) {
+			transform.Rotate (Vector2.up, Time.deltaTime * TurningSpeed, Space.World);
+			TurningValue += 1;
+
+		}
+
+	}
+}
