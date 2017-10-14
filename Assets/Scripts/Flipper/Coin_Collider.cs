9bfc757537aac76719810ac40bd6923193c970b9
Timo <timo@mirvr.com>
2017-10-14 21:48:02 +0200

    Scene for Coin animation now plays if a coin is in the coin collider. Before and after the coin is disabled

--- Assets/Scripts/Flipper/Coin_Collider.cs
+++ Assets/Scripts/Flipper/Coin_Collider.cs
@@ -5,23 +5,41 @@ using UnityEngine;
 public class Coin_Collider : MonoBehaviour {
 
     public Animator Animator;
+    public GameObject Coin;
+    public float coinAnimationDuration = 0f;
     private int coinsInserted;
+    private bool animationStarted = false;
+    private float animationTime = 0f;
 
 	// Use this for initialization
 	void Start () {
-		
+        Coin.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (animationStarted)
+        {
+            animationTime += Time.deltaTime;
+
+            if (animationTime > coinAnimationDuration)
+            {
+                animationStarted = false;
+                animationTime = 0f;
+                Coin.SetActive(false);
+            }
+        }
 	}
 
     void OnTriggerEnter(Collider maybeCoin)
     {
+        Debug.Log("Collided with someting at the Coin insertion");
         if (maybeCoin.tag == "Coin")
         {
+            Debug.Log("Collided with Coin");
+            Coin.SetActive(true);
             Animator.SetTrigger("Coin_insert");
+            animationStarted = true;
         }
     }
 }
