d56b88f16c938d0b248da7f5c6a7caa131c15c3e
Timo <timo@mirvr.com>
2017-10-14 16:14:40 +0200

    Added Spring to flipper plates, added functionality, added better physics

--- Assets/Scripts/Flipper/Flipper_Manager.cs
+++ Assets/Scripts/Flipper/Flipper_Manager.cs
@@ -3,61 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Flipper_Manager : MonoBehaviour {
-
-    public float timeAfterLaunchAnimation = 0.5f;
-    public float forceToBallAtLaunch = 1f;
-    public Transform forceVector;
-    public Rigidbody Ball;
-    private Animator Animator;
-    private float timeSinceLaunchStart = 0;
-    private bool launchAnimationStarted = false;
     
+    private Animator Animator;
 
-	// Use this for initialization
-	void Start () {
+    public float springStrength = 100;
+
+    [SerializeField]
+    private Rigidbody rightFlipper = null;
+
+    [SerializeField]
+    private Rigidbody leftFlipper = null;
+
+    // Use this for initialization
+    void Start () {
         Animator = GetComponent<Animator>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            Animator.SetTrigger("Launch");
-            launchAnimationStarted = true;
-        }
 
+    // Update is called once per frame
+    void Update()
+    {
         if (Input.GetKeyDown(KeyCode.A))
         {
-            Animator.SetBool("Flipperstick_left", true);
+            leftFlipper.GetComponent<HingeJoint>().spring = new JointSpring { targetPosition = 50, spring = springStrength, damper = 2 };
         }
 
         if (Input.GetKeyUp(KeyCode.A))
         {
-            Animator.SetBool("Flipperstick_left", false);
+            leftFlipper.GetComponent<HingeJoint>().spring = new JointSpring { targetPosition = -40, spring = springStrength, damper = 2 };
         }
 
         if (Input.GetKeyDown(KeyCode.D))
         {
-            Animator.SetBool("Flipperstick_right", true);
+            rightFlipper.GetComponent<HingeJoint>().spring = new JointSpring { targetPosition = -50, spring = springStrength, damper = 2 };
         }
 
         if (Input.GetKeyUp(KeyCode.D))
         {
-            Animator.SetBool("Flipperstick_right", false);
+            rightFlipper.GetComponent<HingeJoint>().spring = new JointSpring { targetPosition = 40, spring = springStrength, damper = 2 };
         }
 
 
-        if (launchAnimationStarted)
-        {
-            timeSinceLaunchStart += Time.deltaTime;
-
-            if (timeSinceLaunchStart > timeAfterLaunchAnimation)
-            {
-                launchAnimationStarted = false;
-                timeSinceLaunchStart = 0f;
-
-                Ball.AddForce(forceVector.rotation.ToEulerAngles() * forceToBallAtLaunch);
-            }
-        }
+        
     }
 }
---
+++ Assets/Scripts/Flipper/Gameover_Respawn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gameover_Respawn : MonoBehaviour {
+
+    public Transform Ball;
+    public Transform ballOrigin;
+	
+	void Update () {
+		
+	}
+
+
+    void OnTriggerEnter()
+    {
+        Ball.transform.position = new Vector3(ballOrigin.transform.position.x, ballOrigin.transform.position.y, ballOrigin.transform.position.z);
+    }
+}
