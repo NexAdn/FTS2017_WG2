3ec3ed5b2ddb9cea6d86dc0c8a8d1c00ca6bdfd9
Timo <timo@mirvr.com>
2017-10-14 23:33:46 +0200

    Deleted some garbage

--- Assets/Scripts/Flipper/Flipper_Manager.cs
+++ Assets/Scripts/Flipper/Flipper_Manager.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Flipper_Manager : MonoBehaviour {
-    
-    private Animator Animator;
 
     public float springStrength = 100;
 
@@ -13,11 +11,6 @@ public class Flipper_Manager : MonoBehaviour {
 
     [SerializeField]
     private Rigidbody leftFlipper = null;
-
-    // Use this for initialization
-    void Start () {
-        Animator = GetComponent<Animator>();
-    }
 
     // Update is called once per frame
     void Update()
